456cd93e07f8b1817eda1019d0f923aa0a503976
Galina Popova <g89117301783@gmail.com>
2021-03-13 15:42:41 +0300

    Добавит шапку для декстопной, планшетной и мобильной версии

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+// MENU
+var menuNav = document.querySelector(".menu-nav");
+var menuNavToggle = document.querySelector(".menu-nav__toggle");
+
+menuNav.classList.remove("menu-nav-nojs");
+
+menuNav.classList.add("menu-nav--closed");
+
+menuNavToggle.addEventListener("click", () => {
+    if (menuNav.classList.contains("menu-nav--closed")) {
+      menuNav.classList.remove("menu-nav--closed");
+      menuNav.classList.add("menu-nav--opened");
+    } else {
+      menuNav.classList.add("menu-nav--closed");
+      menuNav.classList.remove("menu-nav--opened");
+    }
+  }
+);
